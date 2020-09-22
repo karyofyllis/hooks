763854dcf49ae1d82989db86c5a6a6e564cdadf7
karyofyllis <karyofyllisgiorgos@gmail.com>
2020-09-22 09:15:06 +0300

    fix import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import axios from "axios";
 
 const useToggle = (initialValue = false) => {
