6c061801839e4074e16c6cb626cbcdaf537fcec1
karyofyllis <karyofyllisgiorgos@gmail.com>
2020-09-22 08:49:50 +0300

    Add more hooks

--- index.js
+++ index.js
@@ -44,8 +44,155 @@ const useForm = (initialValues) => {
 };
 
 
+function useProgress(
+    startedCallback,
+    successCallback,
+    errorCallback,
+    monitored,
+    dbg
+) {
+    const inProgress = useRef(monitored.isLoading);
+
+    useEffect(() => {
+        const { isLoading, error, result } = monitored;
+        dbg &&
+        console.log(
+            `Effect executing for ${dbg} { isLoading: ${isLoading}, inProgress.current: ${inProgress.current}, error: ${error} }`
+        );
+
+        // If loading just started
+        if (isLoading && !inProgress.current) {
+            inProgress.current = true;
+        }
+
+        if (inProgress.current) {
+            if (isLoading) {
+                startedCallback && startedCallback();
+            } else {
+                inProgress.current = false;
+                if (error) {
+                    // Loading has finished and an error was dispatched
+                    errorCallback && errorCallback(error);
+                } else {
+                    // Loading has finished successfully
+                    successCallback && successCallback(result);
+                }
+            }
+        }
+    }, [monitored, startedCallback, successCallback, errorCallback, dbg]);
+}
+
+// Creates the entity progress hooks
+const useEntityProgress = (context, onClose) => {
+    const [busy, setBusy] = useState(false);
+    const [error, setError] = useState(null);
+
+    React.useEffect(() => {
+        if (busy) setError(null);
+    }, [busy]);
+
+    // Methods for tracking progress
+    const showLoader = useCallback((_) => setBusy(true), []);
+    const hideLoaderAndClose = useCallback(
+        (_) => {
+            setBusy(false);
+            onClose(true);
+        },
+        [onClose]
+    );
+    const hideLoaderAndError = useCallback((e) => {
+        setBusy(false);
+        setError(e);
+        console.log("Operation failed ", e);
+    }, []);
+
+    // Hooks for showing the busy indicator
+    useProgress(
+        showLoader,
+        hideLoaderAndClose,
+        hideLoaderAndError,
+        context.update
+    );
+    useProgress(
+        showLoader,
+        hideLoaderAndClose,
+        hideLoaderAndError,
+        context.create
+    );
+    useProgress(
+        showLoader,
+        hideLoaderAndClose,
+        hideLoaderAndError,
+        context.delete
+    );
+
+    return [busy, error];
+};
+
+const useFetch = (url, initialState, pollTimeout) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    const getData = useCallback((url, callback) => {
+        axios
+            .get(url, getAxiosDefaultConfig())
+            .then((res) => {
+                setData(res.data);
+                setError(null);
+                callback && callback();
+            })
+            .catch((ex) => {
+                setError(ex);
+                setData(null);
+                callback && callback();
+            });
+    }, []);
+
+    useEffect(() => {
+        getData(url, () => setIsLoading(false));
+    }, [url, getData]);
+
+    const pollRef = useRef();
+    useEffect(() => {
+        clearInterval(pollRef.current);
+        if (pollTimeout) {
+            pollRef.current = setInterval(() => {
+                getData(url);
+            }, pollTimeout * 1000);
+        }
+        return () => {
+            clearInterval(pollRef.current);
+        };
+    }, [pollTimeout, url, getData]);
+
+    if (!url) {
+        // Fix for not make requests with invalid url
+        // returns the default state
+        return [false, initialState, "", setData];
+    }
+    return [isLoading, data || initialState, error, setData];
+};
+
+const useFiltered = (array, query, filterProps) => {
+    return useMemo(() => {
+        try {
+            return array.filter((x) => {
+                const arrayOfProps = filterProps.map((prop) => x[prop]);
+                return arrayOfProps.find((value) =>
+                    value.toLowerCase().includes(query.toLowerCase())
+                );
+            });
+        } catch (e) {
+            return [];
+        }
+    }, [array, filterProps, query]);
+};
 export {
     useToggle,
     useListToggle,
-    useForm
+    useForm,
+    useProgress,
+    useEntityProgress,
+    useFiltered
 }
